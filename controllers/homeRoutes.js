73d4bd58c13d82e4f4e250ec1129a68947041af8
John Lundy <johnlundy94@gmail.com>
2021-09-27 20:51:58 -0600

    added login page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,14 @@
 const router = require("express").Router();
-
+router.get("/", (req, res) => {
+  res.render("homepage");
+});
+router.get("/login", (req, res) => {
+  res.render("login");
+});
+router.get("/profile", (req, res) => {
+  res.render("profile");
+});
+router.get("/project", (req, res) => {
+  res.render("project");
+});
 module.exports = router;
