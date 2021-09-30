13c8da6439135a06f42a36b188f509729312fc97
John Lundy <johnlundy94@gmail.com>
2021-09-30 15:43:47 -0600

    fixed login

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const blogRoutes = require("./blogRoutes");
-router.use("/user", userRoutes);
-router.use("/blog", blogRoutes);
+router.use("/users", userRoutes);
+router.use("/blogs", blogRoutes);
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/blog/:id", async (req, res) => {
+router.get("/blogs/:id", async (req, res) => {
   try {
     const blogData = await Blog.findByPk(req.params.id, {
       include: [
